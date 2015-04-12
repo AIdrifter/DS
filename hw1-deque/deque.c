fde816b59f4d9f99521b102d95b952f236190fe4
root <root@localhost.localdomain>
2015-04-12 23:50:28 +0800

    [Fix] return correct vlaue

--- hw1-deque/deque.c
+++ hw1-deque/deque.c
@@ -40,33 +40,44 @@ int dequeFree(struct deque_t *p){
 
 // shift all element
 int dequePushFront(struct deque_t *p, const void *pObj){
+    if(p->rear==p->cap)
+        return __DS__DEQUE__FULL__;
+
     // shift array
     memcpy(p->array + p->tSize, p->array , p->tSize*p->rear );
 
     // push
     memcpy(p->array, pObj , p->tSize );
     p->rear++;
-    return 0;
+    return __DS__DEQUE__NORMAL__;
 }
 
 // FIFO(normal queue , first in first out)
 int dequePushBack(struct deque_t *p, const void *pObj){
+    if(p->rear==p->cap)
+        return __DS__DEQUE__FULL__;
+
     memcpy(p->array + p->rear*p->tSize, pObj, p->tSize );
     p->rear++;
-	return 0;
+    return __DS__DEQUE__NORMAL__;
 }
 
 int dequePopFront(struct deque_t *p, void *pRet){
+    if(p->rear==0)
+        return __DS__DEQUE__EMPTY__;
+
     memcpy(pRet, p->array, p->tSize);
     memcpy(p->array, p->array + p->tSize , p->tSize*(p->rear-1) );
     p->rear--;
-    return 0;
+    return __DS__DEQUE__NORMAL__;
 }
 
 int dequePopBack(struct deque_t *p, void *pRet){
+    if(p->rear==0)
+        return __DS__DEQUE__EMPTY__;
     memcpy(pRet, p->array + p->tSize*(p->rear-1), p->tSize);
     p->rear--;
-    return 0;
+    return __DS__DEQUE__NORMAL__;
 }
 
 int dequeEmpty(struct deque_t *p){
@@ -74,13 +85,18 @@ int dequeEmpty(struct deque_t *p){
 }
 
 int dequeFront(struct deque_t *p, void *pRet){
+    if(p->rear==0)
+        return __DS__DEQUE__EMPTY__;
+
     memcpy(pRet, p->array, p->tSize);
-    return 0;
+    return __DS__DEQUE__NORMAL__;
 }
 
 int dequeBack(struct deque_t *p, void *pRet){
+    if(p->rear==0)
+        return __DS__DEQUE__EMPTY__;
     memcpy(pRet, p->array + p->tSize*(p->rear-1), p->tSize);
-    return 0;
+    return __DS__DEQUE__NORMAL__;
 }
 
 size_t dequeCap(struct deque_t *p){
